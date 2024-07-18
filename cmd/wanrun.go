d36955b3e7dc8b13981b8285f5fa1d6192d27897
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-07-18 22:12:03 +0900

    add: health check処理と依存関係を実装、networkの修正

--- cmd/wanrun.go
+++ cmd/wanrun.go
@@ -1,16 +1,18 @@
 package wanruncmd
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	_ "github.com/wanrun-develop/wanrun/internal/db"
+	"github.com/wanrun-develop/wanrun/internal/db"
 )
 
 func Main() {
 	e := echo.New()
 
-	// db := db.NewDB()
+	db := db.NewDB()
+	fmt.Printf("DB info: %v", db)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!!!!")
