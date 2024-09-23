27396e3e1f8088962076d9a93bff0e775b6c8ca4
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-09-23 21:25:34 +0900

    fix: oauth構成

--- migrate/migration_sql/000001_dog_owners.up.sql
+++ migrate/migration_sql/000001_dog_owners.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS dog_owners (
     dog_owner_id serial primary key,
     name varchar(128) not null,
-    email varchar(255) unique not null,
+    email varchar(255) unique,  -- emailはユニークだが、NULLも許可される
+    phone_number varchar(15),  -- phone_numberもNULLを許可する
     image text,
     sex char(1),
-    reg_at timestamp not null,
-    upd_at timestamp not null
+    reg_at timestamp not null default current_timestamp,
+    upd_at timestamp not null default current_timestamp
 );
