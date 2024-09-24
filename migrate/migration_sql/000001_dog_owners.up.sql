6453493b57c57970d7ae2587b355e483dfc8c3d1
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-09-24 23:49:37 +0900

    fix: review

--- migrate/migration_sql/000001_dog_owners.up.sql
+++ migrate/migration_sql/000001_dog_owners.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS dog_owners (
     dog_owner_id serial primary key,
     name varchar(128) not null,
     email varchar(255) unique,  -- emailはユニークだが、NULLも許可される
-    phone_number varchar(15),  -- phone_numberもNULLを許可する
+    phone_number varchar(15) unique,  -- phone_numberもNULLを許可する
     image text,
     sex char(1),
     reg_at timestamp not null,
