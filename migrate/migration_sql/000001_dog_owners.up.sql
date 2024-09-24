99f9e06873dc9098d3d532d29838f70e65443b53
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-09-24 23:50:26 +0900

    fix: OAuthのDB構成 (#59)
    * fix: oauth構成
    
    * fix
    
    * fix: review
    
    * fix: review

--- migrate/migration_sql/000001_dog_owners.up.sql
+++ migrate/migration_sql/000001_dog_owners.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS dog_owners (
     dog_owner_id serial primary key,
     name varchar(128) not null,
-    email varchar(255) unique not null,
+    email varchar(255) unique,  -- emailはユニークだが、NULLも許可される
+    phone_number varchar(15) unique,  -- phone_numberもNULLを許可する
     image text,
     sex char(1),
     reg_at timestamp not null,
--- migrate/migration_sql/000010_auth_dog_owners.up.sql
+++ migrate/migration_sql/000010_auth_dog_owners.up.sql
@@ -1,7 +1,14 @@
+CREATE TYPE grant_type_enum AS ENUM ('PASSWORD', 'OAUTH');
+
 CREATE TABLE IF NOT EXISTS auth_dog_owners (
     auth_dog_owner_id serial primary key,
     dog_owner_id bigint not null,
-    password varchar(256),
-    grant_type int not null,
-    login_at timestamp
+    password varchar(256),  -- パスワード認証用のパスワード。OAuth認証の場合はNULL。
+    email varchar(255),  -- パスワード認証で使うemail。OAuth認証の場合はNULL。
+    phone_number varchar(15),  -- パスワード認証で使うphone_number。OAuth認証の場合はNULL。
+    grant_type grant_type_enum not null,  -- 認証方式のEnum型 (PASSWORD または OAUTH)
+    login_at timestamp,
+
+   -- grant_typeが'PASSWORD'の場合にのみemailかphone_numberのどちらかが必須
+    CHECK ((grant_type = 'PASSWORD' AND (email IS NOT NULL OR phone_number IS NOT NULL)) OR grant_type = 'OAUTH')
 );
